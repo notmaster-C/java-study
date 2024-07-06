af75bd82cb8f8b7a53c38d1599ab13e573a467d3
notmastr <notmastr@163.com>
2024-07-06 23:58:14 +0800

    [new] baidu 121

--- src/main/java/org/com/algorithm/Solution.java
+++ src/main/java/org/com/algorithm/Solution.java
@@ -340,4 +340,20 @@ public class Solution {
         }
         return x;
     }
+
+    /**
+     * 121. 买卖股票的最佳时机
+     */
+    public int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxprofit) {
+                    maxprofit = profit;
+                }
+            }
+        }
+        return maxprofit;
+    }
 }
